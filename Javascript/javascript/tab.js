10281d9c7be6c02f2952ccf93d9691a465cbebd5
bomin-1206 <gbm4655@naver.com>
2023-03-14 11:31:06 +0900

    Javascript_Eventbubbling-0314

--- Javascript/javascript/tab.js
+++ Javascript/javascript/tab.js
@@ -1,12 +1,21 @@
-var button = $('.tab-button') /* 똑같은 주소를 여러번쓰면 별로 좋지않아서 변수에다가 담아서 사용 */
+var button =$(".tab-button"); /* 똑같은 주소를 여러번쓰면 별로 좋지않아서 변수에다가 담아서 사용 */
 
-for (let i = 0; i < $('.tab-button').length; i++) { /* 반복문 안에 변수는 let사용 */
-    button.eq(i).on('click', function() {
-        button.removeClass('orange');
-        button.eq(i).addClass('orange');
-        $('.tab-content').removeClass('show');
-        $('.tab-content').eq(i).addClass('show');
-    })
+// for (let i = 0; i < $(".tab-button").length; i++) {
+//   /* 반복문 안에 변수는 let사용 */
+//   button.eq(i).on("click", function () {
+//     openTab(i);
+//   });
+// }
+
+$('.list').click(function(e) {
+    openTab(e.target.dataset.id)
+})
+
+function openTab(num) {
+  button.removeClass("orange");
+  button.eq(num).addClass("orange");
+  $(".tab-content").removeClass("show");
+  $(".tab-content").eq(num).addClass("show");
 }
 
 // button.eq(0).on('click', function() {
